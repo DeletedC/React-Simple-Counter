c794f120b201b338bd1c5780978536ceebde44f7
DeletedC <32579485+DeletedC@users.noreply.github.com>
2021-11-15 13:24:20 -0500

    Added addition and subtraction buttons, displayed number

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Header from './components/Header';
+
 
 function App() {
+
+  const [counter, setCounter] = useState(0);
+
+  const add = () => {
+    setCounter(counter + 1);
+  }
+
+  const subtract = () => {
+    setCounter(counter - 1);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <button onClick={add}>+</button>
+      <h2>{counter}</h2>
+      <button onClick={subtract}>-</button>
     </div>
   );
 }
