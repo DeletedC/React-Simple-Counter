ebde47a5ecec718a6480be79b0db058f06870cfe
DeletedC <32579485+DeletedC@users.noreply.github.com>
2021-11-16 13:02:01 -0500

    Updated useEffect and moved huzzah message from the console to the DOM

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react';
 const Counter = () => {
 
 	const [counter, setCounter] = useState(0);
+	const [isEven, setIsEven] = useState(false);
 
   const add = () => {
     setCounter(counter + 1);
@@ -13,16 +14,19 @@ const Counter = () => {
   }
 
   useEffect(() => {
-    if (counter % 2 == 0 && counter !== 0) console.log(`${counter} is even! Huzzaaaaah!`);
-  })
+    if (counter % 2 == 0 && counter !== 0) setIsEven(true);
+		else setIsEven(false);
+  }, [counter])
 
   return (
     <div>
 			<div id="counterDiv">
 				<button onClick={subtract}>-</button>
 					<h2 id="counter">{counter}</h2>
-					<button onClick={add}>+</button>     
+					<button onClick={add}>+</button>
 				</div>
+
+				{isEven? <h3 id="huzzah">{`${counter} is even! Huzzaaaaah!`}</h3> : "" }   
     </div>
   )
 }
